b108d24c50636dd56238af5a92b0a9144d49524b
Peter Fox <peter.fox@peterfox.me>
2021-12-14 10:10:06 +0000

    Upgrade tailwind 2 to 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    // By default we allow all styles on compilation
-    // ignore warning about unused styles
-    purge: false,
-
-    // If you wish to purge unused styles you should uncomment the following
-    // and remove the previous purge property above
-    // mode: 'jit',
-    // purge: [
-    //     './resources/views/**/*.blade.php',
-    // ],
+    content: [
+        './resources/views/**/*.blade.php',
+    ],
 
     theme: {
         extend: {
